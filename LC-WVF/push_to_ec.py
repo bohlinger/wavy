89f8d897d8959a0632899724fefe231244d90eb2
bohlinger <patrikb@met.no>
2019-12-05 13:19:53 +0000

    bug fix in push_to_ec, now working

--- LC-WVF/push_to_ec.py
+++ LC-WVF/push_to_ec.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python3
+"""
+needs: pysftp==0.2.8
+bug in version 0.2.9
+"""
+
 import pysftp
 import sys
 import os
+import numpy as np
 sys.path.append(r'/home/patrikb/wavy/wavy')
 from satmod import credentials_from_netrc
 
 # obtain credentials
 host = 'acquisition.ecmwf.int'
 user, pw = credentials_from_netrc(host)
+destination = 'lcwfv_enmi'
 
 # obtain filename
-filelst = np.sort(os.listdir('/lustre/storeA/project/fou/om/LC-WVF'))
+path = '/lustre/storeA/project/fou/om/LC-WVF/'
+filelst = np.sort(os.listdir(path))
 filename = filelst[-1]
+fullname = path + filename
 
 with pysftp.Connection(host=host, username=user, password=pw) as sftp:
-    sftp.cwd('lcwf_enmi')   # alternativt sftp.cd
-    sftp.put(filename)
+    sftp.cwd(destination)
+    sftp.put(fullname)
