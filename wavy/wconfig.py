113aecfc7e5a3446e9e4a41ca6fbbae25fd02b5e
Gaute Hope <gaute.hope@met.no>
2021-06-07 09:29:02 +0200

    wconfig: find config directory
    once dotenv.load_dotenv() is run it will overwrite WAVY_CONFIG for the
    future. it does therefore not make sense to run it on a conditional or
    dependent on what exists in something that is run multiple times.
    Anyway, I think it makes sense to let `.env` override WAVY_CONFIG, and
    fallback to WAVY_CONFIG if available. This way you get a hybrid where
    you can depend on the current directory if you put a `.env` there.
    
    I think this information should go in docs/config.rst with a description
    + spec of the yaml files.

--- wavy/wconfig.py
+++ wavy/wconfig.py
@@ -1,16 +1,40 @@
 import os
 import yaml
 import logging
+
 logger = logging.getLogger(__name__)
+import dotenv
+import xdg
+
+dotenv.load_dotenv()
+
+
+def __get_confdir__():
+    """
+    Configuration directory is specified through:
+
+    .env
+    WAVY_CONNFIG environment variable
+    XDG configuration directory (wavy)
+    """
+    c = os.getenv('WAVY_CONFIG', None)
+    if c is None:
+        c = os.path.join(xdg.xdg_config_home(), 'wavy')
+
+        if not os.path.exists(c):
+            c = None
+
+    logger.debug('config directory: %s' % c)
+    return c
 
 
 def load_or_default(name):
     """
     Load the YAML configuration file from the current directory or use default. The approach is threefold and shown in order:
-    1. check if env 'WAVY_CONFIG' is set
-    2. check if .env exists
-    3. check if a config folder exists using xdg
-    4. fall back to default files within the package
+
+    1. check if env 'WAVY_CONFIG' is set or specified in .env
+    2. check if a config folder exists using xdg
+    3. fall back to default files within the package
 
     e.g.:
 
@@ -20,23 +44,14 @@ def load_or_default(name):
 
     """
     logging.debug('attempting to load: %s..' % name)
-    # get envs for wavy
-    # 1. check if already in envs
-    confdir = os.getenv('WAVY_CONFIG')
-    # 2. if not check if envs are in .env
-    if confdir is None:
-        from dotenv import load_dotenv
-        load_dotenv()
-        confdir = os.getenv('WAVY_CONFIG')
-    # 3. user wide, xdg
-    import xdg
-    confdir = os.path.join(xdg.xdg_config_home(),'wavy')
+
+    confdir = __get_confdir__()
 
     try:
-        #with open(name, 'r') as s:
         if confdir is not None:
-            filestr = os.path.join(confdir,name)
-        else: filestr = name
+            filestr = os.path.join(confdir, name)
+        else:
+            raise FileNotFoundError()
         with open(filestr, 'r') as s:
             return yaml.safe_load(s)
 
