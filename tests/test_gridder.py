5951252e492bc0a32751520507f80dc2fb684896
Patrik Bohlinger <patrikb@pc5591.pc.met.no>
2022-08-10 12:49:28 +0200

    reorganized sat files for test_gridder and increased resolution for longer duration

--- tests/test_gridder.py
+++ tests/test_gridder.py
@@ -6,11 +6,11 @@ import pytest
 from wavy.satmod import satellite_class as sc
 from wavy.gridder import gridder_class as gc
 
-def test_gridder(tmpdir,test_data):
-    sco = sc(sdate="2020-11-1",edate="2020-11-3",region="NordicSeas",
-             path_local=str(test_data)+'/*.nc')
+def test_gridder(test_data):
+    sco = sc(sdate="2020-11-1",edate="2020-11-3",region="global",
+             path_local=str(test_data/"L3"))
     bb = (-170,170,-75,75)
-    res = (2,2)
+    res = (.5,.5)
     print("initialize")
     gco = gc(oco=sco,bb=bb,res=res)
     print("assign obs")
