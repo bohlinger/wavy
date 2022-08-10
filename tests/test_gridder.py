4fb2264628b0b2ee3647805ff8e7b5310eb9bab5
Gaute Hope <gaute.hope@met.no>
2022-08-10 13:40:26 +0200

    benchmark highres as well

--- tests/test_gridder.py
+++ tests/test_gridder.py
@@ -6,7 +6,7 @@ import pytest
 from wavy.satmod import satellite_class as sc
 from wavy.gridder import gridder_class as gc
 
-def test_gridder_highres(test_data):
+def test_gridder_highres(test_data, benchmark):
     sco = sc(sdate="2020-11-1",edate="2020-11-3",region="global",
              path_local=str(test_data/"L3"))
     bb = (-170,170,-75,75)
@@ -16,7 +16,7 @@ def test_gridder_highres(test_data):
     print("assign obs")
     ovals,Midx = gco.get_obs_grid_idx()
     print("compute metric on grid")
-    var_gridded = gco.apply_metric(Midx,ovals,metric="mean")
+    var_gridded = benchmark(gco.apply_metric, Midx,ovals,metric="mean")
 
 def test_gridder_lowres(test_data, benchmark):
     sco = sc(sdate="2020-11-1",edate="2020-11-3",region="global",
