db9d36e14c5a8a665349505f610fd24dca21c50e
Patrik Bohlinger <patrikb@pc5591.pc.met.no>
2022-09-07 15:27:16 +0200

    bugfix in assert in test_multisat

--- tests/test_multisat.py
+++ tests/test_multisat.py
@@ -5,5 +5,5 @@ from wavy.multisat import multisat_class as ms
 def test_multisat(test_data):
     mso = ms(sdate="2020-11-1",edate="2020-11-3",region="NordicSeas",
               mission=['s3a','s3b'], path_local=str(test_data/"L3"))
-    assert len(list(vars(mso).keys())) == 14
+    assert len(list(vars(mso).keys())) == 15
     assert len(list(mso.ocos)) >= 1
