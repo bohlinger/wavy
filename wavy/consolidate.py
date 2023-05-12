435f6dd431701fdcb61a791d3fb144ec539d36a9
Patrik Bohlinger <patrikb@met.no>
2023-05-12 23:09:19 +0200

    adjusted consolidate to xarray use

--- wavy/consolidate.py
+++ wavy/consolidate.py
@@ -12,9 +12,7 @@ from datetime import datetime
 import xarray as xr
 
 # own imports
-from wavy.utils import flatten
-from wavy.satmod import satellite_class
-from wavy.insitumod import insitu_class
+from wavy.satmod import satellite_class as sco
 from wavy.wconfig import load_or_default
 from wavy.quicklookmod import quicklook_class_sat as qls
 # ---------------------------------------------------------------------#
@@ -24,19 +22,20 @@ variable_info = load_or_default('variable_def.yaml')
 
 # --- global functions ------------------------------------------------#
 
-def consolidate_scos(scos):
+
+def consolidate_ocos(ocos):
     """
     consolidate sco.vars: 
         'sea_surface_wave_significant_height', 'time',
         'latitude', 'longitude', 'datetime'
     """
-    das = [sco.vars[sco.varalias] for sco in scos]
-    ds = xr.concat(das,dim='time').to_dataset()
+    das = [oco.vars[oco.varalias] for oco in ocos]
+    ds = xr.concat(das, dim='time').to_dataset()
     return ds
 
 def find_valid_oco(ocos):
     state = False
-    for i,o in enumerate(ocos):
+    for i, o in enumerate(ocos):
         if 'vars' in vars(o).keys():
             state = True
             break
@@ -54,7 +53,7 @@ class consolidate_class(qls):
     '''
     Class to handle multiple satellite_class objects
     '''
-    def __init__(self,ocos):
+    def __init__(self, ocos):
         print('# ----- ')
         print(" ### Initializing consolidate_class object ###")
         print(" ")
@@ -66,15 +65,13 @@ class consolidate_class(qls):
         self.units = ocos[i].units
         self.sd = ocos[i].sd
         self.ed = ocos[i].ed
-        self.vars = consolidate_scos(ocos)
+        self.vars = consolidate_ocos(ocos)
         self.obsname = 'consolidated-obs'
         self.obstype = 'consolidated-obs'
         self.label = 'consolidated-obs'
-        self.mission = 'mission'
-        self.product = 'product'
-        self.provider = 'provider'
-        self.nID = 'nID'
-        self.sensor = 'sensor'
+        self.mission = 'consolidated-missions'
+        self.nID = 'consolidated-nIDs'
+        self.sensor = 'consolidated-sensors'
         print(" ")
         print(" ### consolidate_class object initialized ###")
         print('# ----- ')
@@ -88,8 +85,6 @@ class consolidate_class(qls):
             self.obstype = kwargs.get('obstype')
         if kwargs.get('product') is not None:
             self.product = kwargs.get('product')
-        if kwargs.get('provider') is not None:
-            self.provider = kwargs.get('provider')
         if kwargs.get('nID') is not None:
             self.nID = kwargs.get('nID')
         if kwargs.get('sensor') is not None:
