6fb548305caaf9e0d80c997e882e0575f8a849de
Patrik Bohlinger <patrikb@met.no>
2023-08-10 09:15:01 +0200

    added possibility to consolidate collocation class objects following a suggestion from Malek Ghantous

--- tests/test_consolidate.py
+++ tests/test_consolidate.py
@@ -8,10 +8,10 @@ from wavy.consolidate import consolidate_class as cs
 from wavy.satmod import satellite_class as sc
 
 def test_consolidate_scos(test_data, benchmark):
-    sco1 = sc(sdate="2020-11-1 12",region="NordicSeas",
+    sco1 = sc(sdate="2020-11-1 12", region="NordicSeas",
               mission='s3a', path_local=str(test_data/"L3"))
-    sco2 = sc(sdate="2020-11-1 13",region="NordicSeas",
+    sco2 = sc(sdate="2020-11-1 13", region="NordicSeas",
               mission='s3b', path_local=str(test_data/"L3"))
-    cso = cs([sco1,sco2])
+    cso = cs([sco1, sco2])
     assert len(list(vars(cso).keys())) >= 8
     assert len(list(cso.vars.keys())) == 6
--- wavy/consolidate_collocations.py
+++ wavy/consolidate_collocations.py
@@ -15,7 +15,8 @@ from wavy.utils import flatten
 from wavy.satmod import satellite_class
 from wavy.insitumod import insitu_class
 from wavy.wconfig import load_or_default
-from wavy.quicklookmod import quicklook_class_sat as qls
+#from wavy.quicklookmod import quicklook_class_sat as qls
+from wavy.collocmod import collocation_class as qls
 # ---------------------------------------------------------------------#
 
 # read yaml config files:
@@ -26,41 +27,6 @@ variable_info = load_or_default('variable_info.yaml')
 def consolidate_scos(scos):
     """
     consolidate sco.vars: 
-        'sea_surface_wave_significant_height', 'time',
-        'latitude', 'longitude', 'datetime'
-    """
-    varlst = []
-    lonlst = []
-    latlst = []
-    timelst = []
-    dtimelst = []
-    for sco in scos:
-        varlst.append(sco.vars[sco.stdvarname])
-        lonlst.append(sco.vars['longitude'])
-        latlst.append(sco.vars['latitude'])
-        timelst.append(sco.vars['time'])
-        dtimelst.append(sco.vars['datetime'])
-    # flatten all, make arrays
-    varlst = np.array(flatten(varlst))
-    lonlst = np.array(flatten(lonlst))
-    latlst = np.array(flatten(latlst))
-    timelst = np.array(flatten(timelst))
-    dtimelst = np.array(flatten(dtimelst))
-    # sort according to time
-    idx = np.argsort(timelst)
-    # make dict
-    vardict = {
-            sco.stdvarname: varlst[idx],
-            'longitude': lonlst[idx],
-            'latitude': latlst[idx],
-            'time': timelst[idx],
-            'time_unit': sco.vars['time_unit'],
-            'datetime': dtimelst[idx]}
-    return vardict
-
-def consolidate_ccos(ccos):
-    """
-    consolidate sco.vars:
         'sea_surface_wave_significant_height', 'time',
         'latitude', 'longitude', 'datetime'
     """
@@ -78,24 +44,21 @@ def consolidate_ccos(ccos):
     timelst = []
     dtimelst = []
     # import pdb; pdb.set_trace()
-    for cco in ccos:
-        print(cco)
-        print(type(cco))
-        print(cco.vars.keys())
+    for sco in scos:
         # varlst.append(sco.vars[sco.stdvarname])
-        model_lonlst.append(cco.vars['model_lons'])
-        model_latlst.append(cco.vars['model_lats'])
-        model_valueslst.append(cco.vars['model_values'])
-        obs_lonlst.append(cco.vars['obs_lons'])
-        obs_latlst.append(cco.vars['obs_lats'])
-        obs_valueslst.append(cco.vars['obs_values'])
-        valid_datelst.append(cco.vars['valid_date'])
-        datetimelst.append(cco.vars['datetime'])
-        distancelst.append(cco.vars['distance'])
-        collocation_idx_xlst.append(cco.vars['collocation_idx_x'])
-        collocation_idx_ylst.append(cco.vars['collocation_idx_y'])
-        timelst.append(cco.vars['time'])
-        dtimelst.append(cco.vars['datetime'])
+        model_lonlst.append(sco.vars['model_lons'])
+        model_latlst.append(sco.vars['model_lats'])
+        model_valueslst.append(sco.vars['model_values'])
+        obs_lonlst.append(sco.vars['obs_lons'])
+        obs_latlst.append(sco.vars['obs_lats'])
+        obs_valueslst.append(sco.vars['obs_values'])
+        valid_datelst.append(sco.vars['valid_date'])
+        datetimelst.append(sco.vars['datetime'])
+        distancelst.append(sco.vars['distance'])
+        collocation_idx_xlst.append(sco.vars['collocation_idx_x'])
+        collocation_idx_ylst.append(sco.vars['collocation_idx_y'])
+        timelst.append(sco.vars['time'])
+        dtimelst.append(sco.vars['datetime'])
     # flatten all, make arrays
     # varlst = np.array(flatten(varlst))
     model_lonlst = np.array(flatten(model_lonlst))
@@ -116,29 +79,26 @@ def consolidate_ccos(ccos):
     idxvd = np.argsort(valid_datelst)
     # make dict
     vardict = {
-        # cco.stdvarname:varlst[idx],
-        "valid_date": valid_datelst[idxvd],
-        "time": timelst[idx],
-        'time_unit': cco.vars['time_unit'],
-        "datetime": dtimelst[idx],
-        "distance": distancelst[idx],
-        "model_values": model_valueslst[idx],
-        "model_lons": model_lonlst[idx],
-        "model_lats": model_latlst[idx],
-        "obs_values": obs_valueslst[idx],
-        "obs_lons": obs_lonlst[idx],
-        "obs_lats": obs_latlst[idx],
-        "longitude": obs_lonlst[idx],
-        "latitude": obs_latlst[idx],
-        cco.stdvarname: obs_valueslst[idx],
-        "collocation_idx_x": collocation_idx_xlst[idx],
-        "collocation_idx_y": collocation_idx_ylst[idx],
+        # sco.stdvarname:varlst[idx],
+        "valid_date":valid_datelst[idxvd],
+        "time":timelst[idx],
+        'time_unit':sco.vars['time_unit'],
+        "datetime":dtimelst[idx],
+        "distance":distancelst[idx],
+        "model_values":model_valueslst[idx],
+        "model_lons":model_lonlst[idx],
+        "model_lats":model_latlst[idx],
+        "obs_values":obs_valueslst[idx],
+        "obs_lons":obs_lonlst[idx],
+        "obs_lats":obs_latlst[idx],
+        "collocation_idx_x":collocation_idx_xlst[idx],
+        "collocation_idx_y":collocation_idx_ylst[idx],
         }
     return vardict
 
 def find_valid_oco(ocos):
     state = False
-    for i, o in enumerate(ocos):
+    for i,o in enumerate(ocos):
         if 'vars' in vars(o).keys():
             state = True
             break
@@ -156,7 +116,7 @@ class consolidate_class(qls):
     '''
     Class to handle multiple satellite_class objects
     '''
-    def __init__(self, ocos, obj_type='observation'):
+    def __init__(self,ocos):
         print('# ----- ')
         print(" ### Initializing consolidate_class object ###")
         print(" ")
@@ -164,9 +124,11 @@ class consolidate_class(qls):
         self.ocos = ocos
         self.varalias = ocos[i].varalias
         self.stdvarname = ocos[i].stdvarname
+        #self.varname = ocos[i].varname
         self.units = ocos[i].units
         self.sdate = ocos[i].sdate
         self.edate = ocos[i].edate
+        self.vars = consolidate_scos(ocos)
         self.obsname = 'consolidated-obs'
         self.obstype = 'consolidated-obs'
         self.label = 'consolidated-obs'
@@ -175,19 +137,16 @@ class consolidate_class(qls):
         self.provider = 'provider'
         self.nID = 'nID'
         self.sensor = 'sensor'
-        if obj_type is "observation":
-            self.vars = consolidate_scos(ocos)
-            #self.region = ocos[i].region
-            self.varname = ocos[i].varname
-        elif obj_type is "consolidation":
-            self.vars = consolidate_ccos(ocos)
-            self.model = ocos[i].model
-            #self.region = ocos[i].region
+        self.model = ocos[i].model
+        self.region = ocos[i].region
+        # self.product = ocos[i].product
+        # self.provider = ocos[i].provider
+        
         print(" ")
-        print(" ### consolidate_class object initialized ###")
-        print('# ----- ')
+        print (" ### consolidate_class object initialized ###")
+        print ('# ----- ')
 
-    def rename_consolidate_object_parameters(self, **kwargs):
+    def rename_consolidate_object_parameters(self,**kwargs):
         if kwargs.get('obsname') is not None:
             self.obsname = kwargs.get('obsname')
         if kwargs.get('mission') is not None:
