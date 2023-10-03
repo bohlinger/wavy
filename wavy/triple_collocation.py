202f839e0ba7a124d9d1ebb8808f1723dac4eba7
Patrik Bohlinger <patrikb@met.no>
2023-10-03 14:21:44 +0200

    added import of copy

--- wavy/triple_collocation.py
+++ wavy/triple_collocation.py
@@ -5,9 +5,11 @@ from wavy.utils import find_included_times
 from wavy.validationmod import validate, disp_validation
 import numpy as np
 import pandas as pd
+import copy
 
-def collocate_sat_and_insitu(sco, ico, twin=5): # discuss option for time window
+def collocate_sat_and_insitu(sco, ico, twin=5):
     '''
+     twin in minutes (as in find_included_times)
      create blocks as lists
      compute distances for all instances
      choose min dist for all instances
@@ -17,10 +19,12 @@ def collocate_sat_and_insitu(sco, ico, twin=5): # discuss option for time window
     import datetime
     # create blocks
 
-    dts_src = [datetime.datetime.utcfromtimestamp(t.tolist()/1e9) for t in sco.vars['time'].values]
+    dts_src = [datetime.datetime.utcfromtimestamp(t.tolist()/1e9)
+               for t in sco.vars['time'].values]
     lats_src = sco.vars['lats'].values
     lons_src = sco.vars['lons'].values
-    dts_trgt = [datetime.datetime.utcfromtimestamp(t.tolist()/1e9) for t in ico.vars['time'].values]  # ico.vars
+    dts_trgt = [datetime.datetime.utcfromtimestamp(t.tolist()/1e9)
+                for t in ico.vars['time'].values]  # ico.vars
     dts_blck = []
     lons_blck = []
     lats_blck = []
@@ -29,9 +33,12 @@ def collocate_sat_and_insitu(sco, ico, twin=5): # discuss option for time window
     
     # i is in range of length of in situ data
     for i in range(len(dts_trgt)):
-        # idx = list of indexes from sat data which falls in the time window given by twin (in minutes)
-        # in in-situ datetimes at index i
-        idx = find_included_times(dts_src, dts_trgt[i], twin=twin) # should I take 5 or 10 ?? 
+        """
+        idx = list of indexes from sat data which falls in the
+              time window given by twin (in minutes) in in-situ
+              datetimes at index i
+        """
+        idx = find_included_times(dts_src, dts_trgt[i], twin=twin) 
         # if some indexes are returned
         if len(idx) > 0:
             # list of arrays of matching sat datetime
@@ -40,18 +47,21 @@ def collocate_sat_and_insitu(sco, ico, twin=5): # discuss option for time window
             lats_blck.append(np.array(lats_src)[idx])
             # list of arrays matching sat longitude
             lons_blck.append(np.array(lons_src)[idx])
-            # list of list of indices from sat data which were matched mby in situ 
+            # list of list of indices from sat data which were
+            # matched mby in situ 
             idx_blck.append(idx)
             # list of indices from in-situ data which matched some sat data
             blcks.append(i)
     
     blck_dict = {
-                 # save the obtained matching list of arrays from sat data into the dictionnary
+                 # save the obtained matching list of arrays from
+                 # sat data into the dictionnary
                  'src_datetime': dts_blck,
                  'src_latitude': lats_blck,
                  'src_longitude': lons_blck,
                  'src_idx': idx_blck,
-                 # get datetime, latitude and longitude of in-situ data which matched and save it
+                 # get datetime, latitude and longitude of in-situ data
+                 # which matched and save it
                  'trgt_datetime': [ico.vars['time'].values[b] for b in blcks],
                  'trgt_latitude': [ico.vars['lats'].values[b] for b in blcks],
                  'trgt_longitude': [ico.vars['lons'].values[b] for b in blcks],
@@ -65,18 +75,18 @@ def collocate_sat_and_insitu(sco, ico, twin=5): # discuss option for time window
     blck_dict['min_dst_idx'] = []
     # For each datetime of in-situ data which matched sat data
     for i in range(len(blck_dict['trgt_datetime'])):
-        # lat and lon lists from sat data which matched in-situ data i 
+        # lat and lon lists from sat data which matched in-situ data i
         lon1 = blck_dict['src_longitude'][i]
         lat1 = blck_dict['src_latitude'][i]
-        # create lists of lat and lon of the point i of in-situ data which 
-        # matched sat data, repeated at same length as sat lat, lon list. 
+        # create lists of lat and lon of the point i of in-situ data which
+        # matched sat data, repeated at same length as sat lat, lon list.
         lon2 = [blck_dict['trgt_longitude'][i]]*len(lon1)
         lat2 = [blck_dict['trgt_latitude'][i]]*len(lat1)
-        # compute distance of every point of sat data matched with the corresponding 
-        # in-situ data point i
+        # compute distance of every point of sat data matched with the#
+        # corresponding in-situ data point i
         dist = list(haversineA(lon1, lat1, lon2, lat2)[0])
-    
-        # save the distances lists, the minimum distance each time and the 
+
+        # save the distances lists, the minimum distance each time and the
         # corresponding index inside the sat list matched at point i
         blck_dict['dst'].append(dist)
         blck_dict['min_dst'].append(np.min(dist))
@@ -90,103 +100,95 @@ def collocate_sat_and_insitu(sco, ico, twin=5): # discuss option for time window
                                                  for i in range(len(blck_dict['min_dst_idx']))])
 
     ico_filter.vars = ico_filter.vars.isel(time=[idx for idx in blck_dict['trgt_idx']])
-    # list_vars_to_filter = ['sea_surface_wave_significant_height', 'latitude', 'longitude', 'time', 'datetime']
-    
-    # for v in list_vars_to_filter:
-    
-    #     sco_filter.vars[v] = [sco_filter.vars[v][blck_dict['src_idx'][i]\
-    #                             [blck_dict['min_dst_idx'][i]]]\
-    #                             for i in range(len(blck_dict['min_dst_idx']))]
-    #     ico_filter.vars[v] = [ico_filter.vars[v][idx] for idx in blck_dict['trgt_idx']]
 
     return sco_filter, ico_filter
 
-def remove_nan(A,B,C):
+def remove_nan(A, B, C):
     '''
     Find indexes of nan values in each of three
     lists, and returns the filtered lists
     '''
-    n = len(A) # add test len are equal
-    
+    n = len(A)  # add test len are equal
+
     list_nan_indexes = []
-    
+
     for i in range(n):
-        if np.isnan(A[i]) | np.isnan(B[i]) | np.isnan(C[i])  :
+        if np.isnan(A[i]) | np.isnan(B[i]) | np.isnan(C[i]):
             list_nan_indexes.append(i)
-    
+
     A = [A[i] for i in range(n) if i not in list_nan_indexes]
     B = [B[i] for i in range(n) if i not in list_nan_indexes]
     C = [C[i] for i in range(n) if i not in list_nan_indexes]
-    
-    return A,B,C
-    
-def mixed_second_moment(A,B): 
+
+    return A, B, C
+
+def mixed_second_moment(A, B):
     '''
     Returns the mixed second moment
-    of given lists A and B 
+    of given lists A and B
     '''
     N = len(A)
     mixed_serie_tmp = [A[i]*B[i] for i in range(N)]
 
     return np.sum(mixed_serie_tmp)/N
 
-def covariance(A,B):
+def covariance(A, B):
     '''
-    Returns the covariance of lists 
+    Returns the covariance of lists
     A and B
     '''
-    return mixed_second_moment(A,B) - np.mean(A)*np.mean(B)
+    return mixed_second_moment(A, B) - np.mean(A)*np.mean(B)
 
-def variance_estimates(A,B,C):
+def variance_estimates(A, B, C):
     '''
     Returns the estimate of vairance of random
-    error from list A, according to triple 
+    error from list A, according to triple
     collocation method with the covariance notation
     as in Gruber et al., 2016
     '''
-    return covariance(A,A) - (covariance(A,B)*covariance(A,C))/covariance(B,C)
+    return covariance(A, A) - (covariance(A, B)*covariance(A, C))/covariance(B, C)
 
-def RMSE(A,B,C):
+def RMSE(A, B, C):
     '''
-    Returns the Root Mean Square Error of 
-    error from A 
+    Returns the Root Mean Square Error of
+    error from A
     '''
-    return np.sqrt(variance_estimates(A,B,C))
+    return np.sqrt(variance_estimates(A, B, C))
 
-def SI(A,B,C,ref):
+def SI(A, B, C, ref):
     '''
-    Returns the scatter index from A, with 
+    Returns the scatter index from A, with
     reference to ref measurements
     '''
-    return 100 * RMSE(A,B,C) / np.mean(ref)
+    return 100 * RMSE(A, B, C) / np.mean(ref)
 
-def sensitivity_estimates(A,B,C): 
+def sensitivity_estimates(A, B, C):
     '''
     Returns sensitivity estimates of measurements
     A according to Gruber et al., 2016 definition
     '''
-    return (covariance(A,B)*covariance(A,C)) / covariance(B,C)
+    return (covariance(A, B)*covariance(A, C)) / covariance(B, C)
 
-def SNR(A,B,C,var_err_A):
+def SNR(A, B, C, var_err_A):
     '''
     Returns signal-to-noise ratio of measurements A
     according to Gruber et al., 2016 definition
     '''
-    return sensitivity_estimates(A,B,C)/var_err_A
+    return sensitivity_estimates(A, B, C)/var_err_A
 
-def fMSE(A,B,C,var_err_A):
+def fMSE(A, B, C, var_err_A):
     '''
     Returns fractional Mean Square Error of measurements A
     according to Gruber et al., 2016 definition
     '''
-    return 1/(1+SNR(A,B,C,var_err_A))
+    return 1/(1+SNR(A, B, C, var_err_A))
 
-def SNR_dB(A,B,C,var_err_A):
+def SNR_dB(A, B, C, var_err_A):
     '''
     Returns signal-to-noise ratio in dB of measurements A
     according to Gruber et al., 2016 definition
     '''
-    return 10*np.log10(sensitivity_estimates(A,B,C)/var_err_A)
+    return 10*np.log10(sensitivity_estimates(A, B, C)/var_err_A)
     
 def triple_collocation_validate(results_dict, ref=None):
     '''
@@ -207,13 +209,13 @@ def triple_collocation_validate(results_dict, ref=None):
 
     if len(list(data_sources)) != 3:
         raise Exception("Exactly three data sources must be provided.")
-    
+
     # Check if ref argument is, given and correct
     # if not, takes the first data source as reference
     if ref is None: 
         ref = list(data_sources)[0]
-        print("Since no reference was given,",ref,"was taken as default.")
-  
+        print("Since no reference was given,", ref, "was taken as default.")
+
     if ref not in data_sources:
         print("Incorrect argument `ref` value given, should be one of the keys of `tc_validate`.")
         ref = list(data_sources)[0]
@@ -225,10 +227,10 @@ def triple_collocation_validate(results_dict, ref=None):
     # Iterates on key:value from data_sources dict
     for i,k in enumerate(data_sources):
 
-        # Temporary dictionary to save 
-        # results of current data source 
+        # Temporary dictionary to save
+        # results of current data source
         dict_tmp = {}
-        
+
         # Rotates the data sources at each step i,
         # so A is the ith element of `results_dict.values()`
         ds_list.insert(0, ds_list.pop(i))
@@ -238,18 +240,18 @@ def triple_collocation_validate(results_dict, ref=None):
 
         # Calculates all metrics for A which is, at step i,
         # the ith element of `results_dict.values()`
-        dict_tmp["var_est"] = variance_estimates(A,B,C)
-        dict_tmp["RMSE"] = RMSE(A,B,C)
-        dict_tmp["SI"] = SI(A,B,C, results_dict[ref])
-        dict_tmp["sensitivity"] = sensitivity_estimates(A,B,C)
+        dict_tmp["var_est"] = variance_estimates(A, B, C)
+        dict_tmp["RMSE"] = RMSE(A, B, C)
+        dict_tmp["SI"] = SI(A, B, C, results_dict[ref])
+        dict_tmp["sensitivity"] = sensitivity_estimates(A, B, C)
         #dict_tmp["SNR"] = SNR(A,B,C, dict_tmp["var_est"])
-        dict_tmp["fMSE"] = fMSE(A,B,C, dict_tmp["var_est"])
-        dict_tmp["SNR_dB"] = SNR_dB(A,B,C, dict_tmp["var_est"])
+        dict_tmp["fMSE"] = fMSE(A, B, C, dict_tmp["var_est"])
+        dict_tmp["SNR_dB"] = SNR_dB(A, B, C, dict_tmp["var_est"])
 
         # Save the metric dictionary into validate_dict
         validate_dict[k] = dict_tmp
 
-    validate_dict = {"data_sources":validate_dict}
+    validate_dict = {"data_sources": validate_dict}
     validate_dict["reference_dataset"] = ref
     
     return validate_dict      
@@ -266,7 +268,7 @@ def disp_tc_validation(tc_validate, dec=3):
 
     metrics_names = data_sources_res[ref].keys()
     
-    data = np.array([np.round(list(data_sources_res[k].values()),dec) for k in data_sources_names])
+    data = np.array([np.round(list(data_sources_res[k].values()), dec) for k in data_sources_names])
     data = np.transpose(data)
     
     format_row = "{:>12}" * (len(data_sources_names) + 1)
