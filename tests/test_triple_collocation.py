7088239b9e0aa5f1d8fe1fa3ac43c227434402f9
Patrik Bohlinger <patrikb@met.no>
2023-10-18 12:06:09 +0200

    commented out test templates

--- tests/test_triple_collocation.py
+++ tests/test_triple_collocation.py
@@ -1,10 +1,10 @@
 import pytest
 
-def test_triple_collocation_simulated_data(test_data):
-    # create artificial data
-    # evoke triple collocation
-    # assert type/number of keys/assert the results
+#def test_triple_collocation_simulated_data(test_data):
+#    # create artificial data
+#    # evoke triple collocation
+#    # assert type/number of keys/assert the results
 
-def test_triple_collocation_simulated_data(test_data):
-    # use "real" data
-    # continue as above
+#def test_triple_collocation_simulated_data(test_data):
+#    # use "real" data
+#    # continue as above
