0d816ce41312409783be8bb5d1e8a09e7effadd8
Fabien Collas <fabien.clls@gmail.com>
2024-01-19 11:52:57 +0100

    Added function to apply triple collocation giving wavy objects as input

--- tests/test_triple_collocation.py
+++ tests/test_triple_collocation.py
@@ -1,4 +1,8 @@
 import pytest
+from wavy.insitu_module import insitu_class as ic
+from wavy.satellite_module import satellite_class as sc
+from wavy.model_module import model_class as mc
+import xarray as xr
 import wavy.triple_collocation as tc
 import numpy as np
 
@@ -47,7 +51,7 @@ def test_triple_collocation_simulated_data():
 
 
 def test_ci():
-    
+
     n = 1500
     T = [np.sin(0.2 * i) + 2 for i in range(n)]
     b_x = 1.1
@@ -78,10 +82,48 @@ def test_ci():
 
     assert isinstance(ci_95, dict)
     assert set(dict_data.keys()) == set(ci_95.keys())
-    assert set(['mean','ci_l','ci_u']) == set(ci_95['X'].keys())
+    assert set(['mean', 'ci_l', 'ci_u']) == set(ci_95['X'].keys())
 
 
-# def test_triple_collocation_real_data():
+def test_triple_collocation_wavy_objects(test_data):
 
-#    # use "real" data
-#    # continue as above
+    # Import in-situ data
+    ico = ic(sd='2014-01-01',
+             ed='2018-12-31',
+             nID='history_cmems_NRT',
+             name='Norne')
+    ico.vars = xr.open_dataset(
+                              str(test_data/"triple_collocation/Norne_ico.nc")
+                              )
+    # Import satellite data
+    sco = sc(sd='2014-01-01',
+             ed='2018-12-31',
+             nID='CCIv1_L3',
+             name='multi')
+    sco.vars = xr.open_dataset(
+                              str(test_data/"triple_collocation/Norne_sco.nc")
+                              )
+    # Import model data
+    mco = mc(sd='2014-01-01',
+             ed='2018-12-31',
+             nID='NORA3_hc_waves')
+    mco.vars = xr.open_dataset(
+                              str(test_data/"triple_collocation/Norne_mco.nc")
+                              )
+    # Create dictionary for triple collocation function
+    dict_data = {'insitu': ico, 'satellite': sco, 'model': mco}
+    # Apply triple collocation
+    ref = 'insitu'
+    tc_res = tc.tc_analysis_wavy_objects(dict_data, ref=ref)
+    assert isinstance(tc_res, dict)
+
+    keys_lvl_1 = tc_res.keys()
+    assert 'data_sources' in keys_lvl_1
+    assert 'reference_dataset' in keys_lvl_1
+    assert tc_res['reference_dataset'] in dict_data.keys()
+
+    keys_lvl_2 = tc_res['data_sources'].keys()
+    assert set(keys_lvl_2) == set(dict_data.keys())
+
+    assert ref in keys_lvl_2
+    assert len(tc_res['data_sources'][ref].keys()) == 6
