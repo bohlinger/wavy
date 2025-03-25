7544b60e86f3a72ec8c3a326c04ab9c3b09ef466
Patrik Bohlinger <patrikb@met.no>
2025-03-25 20:03:49 +0100

    added tests on landMask and distance_to_coast filter

--- tests/test_filtermod.py
+++ tests/test_filtermod.py
@@ -5,6 +5,7 @@ import numpy as np
 import os
 from copy import deepcopy
 from wavy.insitu_module import insitu_class as ic
+from wavy.satellite_module import satellite_class as sc
 
 
 def test_filter_runmean(test_data):
@@ -93,3 +94,53 @@ def test_despike_blockStd(test_data):
     new = ico.despike_blockQ(slider=12, sigma=2, chunk_min=6,
                              sampling_rate_Hz=1/1200)
     assert not 'error' in vars(new).keys()
+
+def test_filter_landMask(test_data):
+    sd = "2022-2-1 12"
+    ed = "2022-2-1 12"
+    name = 's3a'
+    varalias = 'Hs'
+    twin = 30
+    nID = 'cmems_L3_NRT'
+    # init satellite_object
+    sco = sc(sd=sd, ed=ed, nID=nID, name=name,
+             varalias=varalias,
+             twin=twin)
+    # read data
+    sco = sco.populate(path=str(test_data/"L3/s3a"))
+    sco = sco.filter_landMask()
+    assert sco.__class__.__name__ == 'satellite_class'
+    # compare number of available variables
+    vlst = list(vars(sco).keys())
+    assert len(vlst) == 20
+    # compare number of available functions
+    dlst = dir(sco)
+    flst = [n for n in dlst if n not in vlst if '__' not in n]
+    assert len(flst) >= 47
+    assert type(sco.vars == 'xarray.core.dataset.Dataset')
+    assert not 'error' in vars(sco).keys()
+
+def test_filter_distance_to_coast(test_data):
+    sd = "2022-2-1 12"
+    ed = "2022-2-1 12"
+    name = 's3a'
+    varalias = 'Hs'
+    twin = 30
+    nID = 'cmems_L3_NRT'
+    # init satellite_object
+    sco = sc(sd=sd, ed=ed, nID=nID, name=name,
+             varalias=varalias,
+             twin=twin)
+    # read data
+    sco = sco.populate(path=str(test_data/"L3/s3a"))
+    sco = sco.filter_distance_to_coast(llim=50000, ulim=1000000)
+    assert sco.__class__.__name__ == 'satellite_class'
+    # compare number of available variables
+    vlst = list(vars(sco).keys())
+    assert len(vlst) == 19
+    # compare number of available functions
+    dlst = dir(sco)
+    flst = [n for n in dlst if n not in vlst if '__' not in n]
+    assert len(flst) >= 47
+    assert type(sco.vars == 'xarray.core.dataset.Dataset')
+    assert not 'error' in vars(sco).keys()
